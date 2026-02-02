cadc47184656ae49689cd486d60cfcf6fbe43253
Rafi Ullah <rafideveloper7@gmail.com>
2026-02-02 09:14:45 +0500

    update vercel

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
   res.json({ message: "API is running" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
