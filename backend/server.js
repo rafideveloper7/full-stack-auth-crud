1375682f68fc44daa97dc773da1d55b99a7e9374
rafideveloper7 <rafideveloper7@gmail.com>
2026-02-18 15:54:42 +0500

    add origin links

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
   "https://frontend-crud-liart.vercel.app", 
+  "https://frontend-crud-liart.vercel.app/", 
   "http://localhost:5173", 
   "http://127.0.0.1:5173"
 ],
