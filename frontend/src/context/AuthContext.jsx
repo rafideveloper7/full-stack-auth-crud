c74db1661e1f86c1f13b371f1914ee0054ba4351
rafideveloper7 <rafideveloper7@gmail.com>
2026-02-19 08:35:03 +0500

    update env

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 // Create context
 export const AuthContext = createContext(null);
 
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const AuthProvider = ({ children }) => {
