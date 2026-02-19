7aa0dcc2f7111f994322bfb8f9fcb4abf7c0d2df
rafideveloper7 <rafideveloper7@gmail.com>
2026-02-19 09:09:11 +0500

    update cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // --- CORS ---
 app.use(cors({
   origin: [
-    "https://todoapi-wine.vercel.app/", 
+    "https://todoapi-wine.vercel.app", 
     "http://localhost:5173", 
     "http://127.0.0.1:5173"
   ],
